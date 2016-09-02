431db1a3e9f229bce150e04383a1dee7643a23fe
WestMan2000 <nikolaev.nikolay@gmail.com>
2016-09-02 12:24:55 -0400

    ...

---
+++ filemanager/src/main/java/corp/wmsoft/android/lib/filemanager/ui/widgets/nav/FileManagerViewPresenterFactory.java
@@ -0,0 +1,23 @@
+package corp.wmsoft.android.lib.filemanager.ui.widgets.nav;
+
+import corp.wmsoft.android.lib.filemanager.di.Injection;
+import corp.wmsoft.android.lib.mvpc.presenter.factory.IMVPCPresenterFactory;
+
+
+/**
+ * Created by admin on 8/8/16 at 3:53 PM
+ *
+ */
+public class FileManagerViewPresenterFactory implements IMVPCPresenterFactory<IFileManagerViewContract.View, IFileManagerViewContract.Presenter> {
+
+
+    public FileManagerViewPresenterFactory() {
+    }
+
+    @Override
+    public IFileManagerViewContract.Presenter create() {
+        return new FileManagerViewPresenter(
+                Injection.provideGetFSOList()
+        );
+    }
+}
